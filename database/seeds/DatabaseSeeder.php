baed270237c937227c7cb01f4b03d33024df71af
unknown <olafkramer2001@gmail.com>
2019-06-04 11:17:55 +0200

    paar code dingen verwijderd

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call( FacilitiesTableSeeder::class);
-        //$this->call( AttractionsTableSeeder::class);
         $this->call( CategorieTableSeeder::class);
     }
 }
