f6106385becd805973c624699758e43e47c2c1dc
unknown <olafkramer2001@gmail.com>
2019-06-11 20:38:05 +0200

    de code wat netter gemaakt + padding toegevoegd

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -41,39 +41,29 @@
                 <div class="dropdown dropleft">
                     <img alt="portaal" data-toggle="dropdown" class="navImg mx-auto d-block rounded-circle" src="{{asset('images/login.png')}}">
                     <div class="dropdown-menu">
-                        {{--hier komen de linkjes voor de authenticate--}}
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
-
-
                         @else
-
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
-
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
-
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
                                 </form>
-
-
                             </div>
-
                         @endguest
                     </div>
                 </div>
