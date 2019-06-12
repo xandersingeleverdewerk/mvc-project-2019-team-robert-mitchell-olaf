233a09d46273700207aa55a4dfc37dca49d57a43
Mitchell2807 <mitchellsmits12@hotmail.com>
2019-06-12 13:29:51 +0200

    begin permissions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,6 +12,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+    public function boot()
+    {
+        //
+        Schema::defaultStringLength(191);
+    }
+
     public function register()
     {
         //
@@ -21,8 +28,5 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
-    }
+
 }
