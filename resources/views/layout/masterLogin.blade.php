1a6eaa53d284d3650f6a2d1de5cd5cb3e26e47dd
unknown <olafkramer2001@gmail.com>
2019-06-12 12:06:02 +0200

    gewerkt aan de mijn pagina

---
+++ resources/views/layout/masterLogin.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Pretpark HappyLand</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('css/scss/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/scss/main.css') }}">
+</head>
+<body>
+<nav class="navigation">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4 navButton">
+                <button class="hamburger btn btn-lg btn-primary" type="button" data-toggle="collapse" data-target="#navigation">
+                    <span class="fas fa-bars"></span>
+                </button>
+            </div>
+            <div class="col-4 col-md-3 logo">
+                <img alt="logo" class="navImg mx-auto d-block rounded-circle" src="{{asset('images/happy.jpg')}}">
+            </div>
+            <div class="col-12 col-md-6 search">
+                <h1 class="navTitle">Pretpark HappyLand</h1>
+                <form id="search" action="#" method="POST">
+                    @csrf
+                    <div class="input-group">
+                        <input class="form-control" type="text" placeholder="Zoeken voor iets">
+                        <div class="input-group-append">
+                            <button class="btn btn-primary" type="submit">Zoeken</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-4 col-md-3 register">
+                <div class="dropdown dropleft">
+                    <img alt="portaal" data-toggle="dropdown" class="navImg mx-auto d-block rounded-circle" src="{{asset('images/login.png')}}">
+                    <div class="dropdown-menu">
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        @endguest
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <ul class="flex-column flex-md-row nav nav-pills nav-justified collapse navbar-collapse" id="navigation">
+            <li class="nav-item">
+                <a class="nav-link {{ Request::url() == url('/mypage') ? 'active' : '' }}" href="{{ url('/mypage') }}">Dashboard</a>
+            </li>
+            <li class="dropdown nav-item nav-link {{ Request::url() == url('/park') ? 'active' : '' }}" data-toggle="dropdown">
+                Het park
+            </li>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="{{url('/park/attractions') }}">Attracties</a>
+                <a class="dropdown-item" href="{{url('/park/restaurants') }}">Restaurants</a>
+                <a class="dropdown-item" href="{{url('/park/stores') }}">Winkels</a>
+            </div>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::url() == url('/gegevens') ? 'active' : '' }}" href="{{ url('/gegevens') }}">Mijn gegevens</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" {{ Request::url() == url('/') ? 'active' : '' }} href="{{ url('/') }}">Terug naar de Website</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+@yield('content')
+
+</body>
+</html>
--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,5 +1,50 @@
-@extends("layout.master")
+@extends("layout.masterLogin")
+
+@php
+    $currentHour = date('G') + 2;
+
+    if($currentHour>=18)
+    {$welcomeMessage = 'Goedenavond';}
+    else if($currentHour>=12)
+    {$welcomeMessage = 'Goedemiddag';}
+    else if($currentHour>=6)
+    {$welcomeMessage = 'Goedemorgen';}
+    else
+    {$welcomeMessage= 'Goedennacht';}
+@endphp
 
 @section('content')
-    Hallo welkom op de mijn pagina
+    <div class="welcomesSection">
+        <div class="container">
+            <div class="jumbotron">
+                <h2 class="Title">{{ $welcomeMessage }} {{ Auth::user()->name }}</h2>
+                <form class="form-inline" action="/reservations.php">
+                    <label for="ticket" class="mr-sm-2">Ticket</label>
+                    <input name="ticket" id="ticekt" type="text" class="form-control mb-2 mr-sm-2">
+                    <label for="quantity" class="mr-sm-2">Aantal</label>
+                    <input name="quantity" id="quantity" type="number" class="form-control mb-2 mr-sm-2">
+                    <button type="submit" class="btn btn-primary mb-2">Reserveren</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="blockSection">
+        <div class="container">
+            <div class="d-flex justify-content-around row">
+                <a class="blockLink" href="#">
+                    <h3 class="blockTitle">Dit is een linkje</h3>
+                </a>
+                <a class="blockLink" href="#">
+                    <h3 class="blockTitle">Dit is een linkje</h3>
+                </a>
+                <a class="blockLink" href="#">
+                    <h3 class="blockTitle">Dit is een linkje</h3>
+                </a>
+                <a class="blockLink" href="#">
+                    <h3 class="blockTitle">Dit is een linkje</h3>
+                </a>
+            </div>
+        </div>
+    </div>
 @endsection
