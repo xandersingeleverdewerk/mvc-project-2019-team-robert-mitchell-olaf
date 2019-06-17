464b1ba57114f1ddac8a4389c030236b2bc3cf70
Mitchell2807 <mitchellsmits12@hotmail.com>
2019-06-17 21:03:25 +0200

    products, stores en storeRules cruds beveiligd

--- resources/views/park/stores/storeRules/show.blade.php
+++ resources/views/park/stores/storeRules/show.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 
+    @can('show storeRules')
     <section class="productsSection">
         <div class="container">
             <div class="d-flex flex">
@@ -44,5 +45,16 @@
             </table>
         </div>
     </section>
+    @endcan
+    @cannot('show storeRules')
+        <div class="alert alert-danger">
+            <ul>
+                U heeft niet de juiste rechten tot dit deel van de site. Keer a.u.b. terug naar de hoofdpagina.
+            </ul>
+        </div>
+        <a href="{{ url('/') }}">
+            <button type="button" class="btn btn-primary">Home</button>
+        </a>
+    @endcannot
 
 @endsection
