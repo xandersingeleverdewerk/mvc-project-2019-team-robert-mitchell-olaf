b630f0e9b2adf91064b0c914f226beb990a94410
unknown <olafkramer2001@gmail.com>
2019-06-20 10:53:26 +0200

    custom 404 error gemaakt

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+@extends("layout.master")
+
+@section('content')
+    <div class="errorSection">
+        <div class="container">
+            <h2>Error</h2>
+
+            <div class="alert alert-danger">
+                Deze pagina bestaat niet. Klik op de knop om terug te gaan naar de Home pagina
+            </div>
+            <a href="{{ back() }}">
+                <button type="button" class="btn btn-primary">Home</button>
+            </a>
+        </div>
+    </div>
+@endsection
--- resources/views/errors/noPermission.blade.php
+++ resources/views/errors/noPermission.blade.php
@@ -1,7 +1,5 @@
 <div class="alert alert-danger">
-    <ul>
-        U heeft niet de juiste rechten tot dit deel van de site. Keer a.u.b. terug naar de hoofdpagina.
-    </ul>
+    U heeft niet de juiste rechten tot dit deel van de site. Keer a.u.b. terug naar de hoofdpagina.
 </div>
 <a href="{{ url('/') }}">
     <button type="button" class="btn btn-primary">Home</button>
